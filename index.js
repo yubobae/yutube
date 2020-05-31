3dd069a9ebb135d6fd514ce79e19faa82a6c6f1d
bobaeya <baeya05@naver.com>
2020-06-01 00:47:35 +0900

    create babel and nodemon settings

--- index.js
+++ index.js
@@ -1,20 +1,16 @@
-const express = require('express');
+import express from 'express';
 const app = express();
 
 const PORT = 4000;
-function handleListening (){
+const handleListening = () =>
     console.log(`Listening on: http://localhost:${PORT}`);
-}
-//응답
-function handleHome(req, res) {
-    res.send("Hello from home");
-}
-//응답
-function handleProfile(req, res) {
-    res.send('you are on my profile!')
-}
-//router 생성.
+
+const handleHome = (req, res) => res.send("Hello from home");
+
+const handleProfile = (req, res) => res.send('you are on my profile!');
+
+
 app.get('/', handleHome);
 app.get('/profile', handleProfile)
-//서버생성
+
 app.listen(PORT, handleListening);
