7201b452c3fa5d7b098d3f3aa9ad07b42c873eb7
bobaeya <baeya05@naver.com>
2020-05-31 22:57:46 +0900

    create express server

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log('hi')
+const express = require('express');
+const app = express();
+
+const PORT = 4000;
+function handleListening (){
+    console.log(`Listening on: http://localhost:${PORT}`);
+}
+//응답
+function handleHome(req, res) {
+    res.send("Hello from home");
+}
+//응답
+function handleProfile(req, res) {
+    res.send('you are on my profile!')
+}
+//router 생성.
+app.get('/', handleHome);
+app.get('/profile', handleProfile)
+//서버생성
+app.listen(PORT, handleListening);
