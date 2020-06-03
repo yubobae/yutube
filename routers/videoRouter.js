86c6ec3cdce7ca1d8cc209766b417c65d2f1bc9e
bobaeya <baeya05@naver.com>
2020-06-04 01:01:43 +0900

    create Controller and router settings

--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,5 +1,13 @@
 import express from "express";
+import routes from "../routes";
+import { videos, upload, videoDetail, editVideo, deleteVdieo } from "../controllers/videoContruller";
 
 const videoRouter = express.Router();
+
+videoRouter.get(routes.videos, videos);
+videoRouter.get(routes.upload, upload);
+videoRouter.get(routes.videoDetail, videoDetail);
+videoRouter.get(routes.editVideo, editVideo);
+videoRouter.get(routes.deleteVideo, deleteVdieo);
 
 export default videoRouter;
